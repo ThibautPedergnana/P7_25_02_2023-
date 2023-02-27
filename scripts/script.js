d9f7e81c8339fb723447ff63a5c01bc711072324
Thibaut Pedergnana <t.pedergnana@Outlook.fr>
2023-02-27 14:59:52 +0100

    search bar done, css incomplete, ingredients missing

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,47 @@
 import { recipes } from "/scripts/utils/recipes.js";
-const recipeSection = document.querySelector(".recipe-section");
-const searchInput = document.getElementById("search");
+// console.log(...recipes);
+const recipeCardTemplate = document.querySelector("[data-recipe-template]");
+const recipeCardContainer = document.querySelector(
+  "[data-recipe-cards-container]"
+);
+const searchInput = document.querySelector("[data-search]");
 
-let users = [];
+let myRecipes = [];
 
 searchInput.addEventListener("input", (e) => {
-  const value = e.target.value;
-  console.log(users);
+  const value = e.target.value.toLowerCase();
+  myRecipes.forEach((recipe) => {
+    const isVisible =
+      recipe.name.toLowerCase().includes(value) ||
+      recipe.description.toLowerCase().includes(value);
+    recipe.element.classList.toggle("hide", !isVisible);
+  });
 });
 
 function init() {
-  users = recipes.map((recipe) => {
-    const Template = new RecipeCard(recipe);
-    recipeSection.appendChild(Template.createRecipeCard());
-    return { name: recipe.name, time: recipe.time };
+  myRecipes = recipes.map((recipe) => {
+    const card = recipeCardTemplate.content.cloneNode(true).children[0];
+    const title = card.querySelector("[data-title]");
+    const time = card.querySelector("[data-time]");
+    const ingredient = card.querySelector("[data-ingredient]");
+    const quantity = card.querySelector("[data-quantity]");
+    const unit = card.querySelector("[data-unit]");
+    const description = card.querySelector("[data-description]");
+
+    title.textContent = recipe.name;
+    time.textContent = recipe.time + " min";
+    ingredient.textContent = recipe.ingredients[0].ingredient + ":";
+    quantity.textContent = recipe.ingredients[0].quantity;
+    unit.textContent = recipe.ingredients[0].unit;
+    description.textContent = recipe.description;
+
+    recipeCardContainer.append(card);
+    return {
+      name: recipe.name,
+      ingredient: recipe.ingredients[0].ingredient,
+      description: recipe.description,
+      element: card,
+    };
   });
 }
 init();
